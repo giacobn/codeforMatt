3b81250602140852c6e071eb3b6607536f8822c6
giacobn <giacobn@comcast.net>
2023-03-26 09:56:02 -0400

    ready

--- api.py
+++ api.py
@@ -1,50 +1,46 @@
-import pyrebase
-import time
-class api_caller: 
-    _config = {
-        "apiKey": "AIzaSyDCSVVTezCeKcScHTc-OuXIm8KhhLRhkL8",
-        "authDomain": "usersignin-fbecc.firebaseapp.com",
-        "projectId": "usersignin-fbecc",
-        "storageBucket": "usersignin-fbecc.appspot.com",
-        "messagingSenderId": "648351672323",
-        "appId": "1:648351672323:web:8cc3e25319c35d0ae34636",
-        "measurementId": "G-KB4P0VTSRZ",
-        "serviceAccount": "serviceAccount.json",
-        "databaseURL": "https://usersignin-fbecc-default-rtdb.firebaseio.com/"
-    }
-    frameCount = 0
-    fireSmokePeopleCount = 0
-    firesmokeCount = 0
-    def __init__(self):
-        pass
-    def uploadFire(image):
-        firebase = pyrebase.initialize_app(api_caller._config)
-        storage = firebase.storage()
-        while True:
-            # Upload the file to Firebase Storage
-            storage.child(image).put(image)
+import cv2
+import requests
 
-            # Refresh the file list in Firebase Storage
-            files = storage.list_files()
-            print("List of files in Firebase Storage:")
-            for file in files:
-                print(file.name)
+class FirebaseStorageUploader:
+    project_id = "usersignin-fbecc"
+    bucket_name = "usersignin-fbecc.appspot.com"
+    access_token = "3ebde5d70fa8171a9832b4d053068452b96a358"
+    fire_count = 0
+    people_count = 0
+    
+    def upload_image(self, image, file_name):
+        # Create the URL for the Firebase Storage upload API
+        url = f"https://firebasestorage.googleapis.com/v0/b/{self.bucket_name}/o?name={file_name}"
 
-            # Wait for a few seconds before repeating the loop
-            time.sleep(1)
-    def count_update(fire,people,frame):
-        api_caller.frameCount+=1
-        if(fire and not(people)):
-            api_caller.firesmokeCount +=1
-        elif(fire and people):
-            api_caller.fireSmokePeopleCount +=1
-        if(api_caller.firesmokeCount>1):
-            api_caller.uploadFire(frame)
-        if(api_caller.fireSmokePeopleCount>0):
-            print("warning")
-            #pushnotification
-            api_caller.uploadFire(frame)
-        if api_caller.frameCount>2:
-            api_caller.frameCount = 0
-            api_caller.fireSmokePeopleCount = 0
-            api_caller.firesmokeCount = 0
+        # Create the request headers with the access token and content type
+        headers = {
+            "Authorization": f"Bearer {self.access_token}",
+            "Content-Type": "image/jpeg"
+        }
+
+        # Encode the OpenCV image as JPEG and send a POST request to the Firebase Storage upload API
+        _, encoded_image = cv2.imencode(".jpg", image)
+        response = requests.post(url, headers=headers, data=encoded_image.tobytes())
+
+        # Print the response from the Firebase Storage upload API
+        print(response.text)
+        
+    def check_fire(self, frame, fire_detected, people_detected):
+        if fire_detected:
+            self.fire_count += 1
+            if self.fire_count >= 2 and self.people_count>=1:
+                # Upload the frame to Firebase Storage
+                file_name = f"fire_{self.fire_count}.jpg"
+                self.upload_image(frame, file_name)
+                print("Warning: Fire detected for two consecutive frames!")
+            elif self.fire_count >= 2 and self.people_count<1:
+                # Upload the frame to Firebase Storage
+                file_name = f"fire_{self.fire_count}.jpg"
+                self.upload_image(frame, file_name)
+                print("Alarm: Fire detected for two consecutive frames!")
+        else:
+            self.fire_count = 0
+        if people_detected:
+            self.people_count += 1
+        else:
+            self.people_count = 0
